8e3a85a26bc70f8f7b12075016b6d850d7e134ec
yonits12 <yonits12@gmail.com>
2020-04-19 23:11:36 +0300

    modularity of functions, except for translate_line

--- PyAssembler/Tutorial1.py
+++ PyAssembler/Tutorial1.py
@@ -1,8 +1,9 @@
 # Global Variables
-
 current_address = 0
 sym_tbl = {}
 
+
+# Helper Functions
 
 def invokeError(message):
    print("ERROR:" + message)
@@ -62,63 +63,30 @@ def update_sym_tbl(label):
    global current_address, sym_tbl
    sym_tbl.update({label: current_address})
 
-def first_step(assembly_file):
-   global current_address, sym_tbl
-   with open(assembly_file,'r') as origin_file:
-      all_lines = origin_file.readlines()
-      for index, line in enumerate(all_lines):
-         if line == '\n' or line[0] == ';': continue
-         all_lines[index] = translate_line(line[:-1] if line[-1] is '\n' else line)                                  # remove \n
-   with open('semi_output.txt', 'w') as outfile:
-      for line in all_lines:
-         outfile.write(line + '\n')                                  # TODO remove \n before submit
-
-def second_step(outfile_name):
-   global current_address, sym_tbl
-   # read almost_binary_line
-   with open('semi_output.txt','r') as intermidiate_file:
-      all_lines = intermidiate_file.readlines()
-      for index, line in enumerate(all_lines):
-         all_lines[index] = fix_line(line[:-1] if line[-1] is '\n' else line)                                  # remove \n
-   with open('cool_' + outfile_name, 'w') as outfile:
-      for line in all_lines:
-         outfile.write(line + '\n')
-   with open(outfile_name, 'w') as outfile:
-      for line in all_lines:
-         outfile.write(line)
-   print("Assembling Done.")
-
-def validate_expression(code_line):
-   tokens = code_line.split(' ')
-   print(tokens)
-
 def check_label(words):
    if words[0][-1] == ':':
       update_sym_tbl(words[0][:-1])
       return words[1:]
    return words
 
-def translate_line(ass_line):
-   global current_address, sym_tbl
+def handle_opcode(words):
    translated_word1 = '0000000000000000'
-   translated_word2 = '0000000000000000'
-   words = ass_line.split(' ')
-   words = check_label(words)
    bin_opcode = switch_opcode(words[0])
    if not bin_opcode:
-      return section_handler(words)
-   translated_word1 = bin_opcode + translated_word1[4:]                                # update opcode bits
+      return True, section_handler(words)
+   return False, bin_opcode + translated_word1[4:]        # update opcode bits
+
+def handle_by_type(words, translated_word1):
+   global current_address
+   translated_word2 = '0000000000000000'
+   current_address+=2
    line_type = len(words)
-   if(line_type == 1):                                # nop
-      current_address+=2
-      return translated_word1
+   if(line_type == 1): return translated_word1        # nop
    addr_mode = switch_addressing_mode(words[-1])
    translated_word1 = translated_word1[:7] + addr_mode[0] + translated_word1[9:]       # update addr_mode bits
    if(line_type == 2):                                # not clr inc dec jmp jne jz sections
       bin_reg = switch_register(addr_mode[1])                                          # check the inner parameter
-      if bin_reg:
-         current_address+=2
-         return translated_word1[:4] + bin_reg + translated_word1[7:]      # update first reg bits
+      if bin_reg: return translated_word1[:4] + bin_reg + translated_word1[7:]      # update first reg bits
       elif addr_mode[1].isnumeric():
          translated_word2 = format(int(addr_mode[1]), '016b')                          # update numeric value bits
       else:
@@ -128,18 +96,23 @@ def translate_line(ass_line):
       if not bin_reg1: invokeError("Invalid first register of 3-type line")
       else: translated_word1 = translated_word1[:4] + bin_reg1 + translated_word1[7:]  # update first reg bits
       bin_reg2 = switch_register(addr_mode[1])
-      if bin_reg2:
-         current_address+=2
-         return translated_word1[:9] + bin_reg2 + translated_word1[12:]                # update second reg bits
+      if bin_reg2: return translated_word1[:9] + bin_reg2 + translated_word1[12:]                # update second reg bits
       elif addr_mode[1].isnumeric():
          translated_word2 = format(int(addr_mode[1]), '016b')                          # update numeric value bits
       else:
          translated_word2 = addr_mode[1]                                               # update placeholder bits
    else:
       invokeError("It is not a valid assembly line (maybe comment or sections).")
-   current_address+=4
+   current_address+=2
    return '' + translated_word1 + '\n' + translated_word2
 
+def translate_line(ass_line):
+   global current_address, sym_tbl
+   words = ass_line.split(' ')
+   words = check_label(words)
+   label_flag, translated_word1 = handle_opcode(words)
+   if label_flag: return translated_word1
+   return handle_by_type(words, translated_word1)
 
 def fix_line(damaged_line):
    global sym_tbl
@@ -162,9 +135,39 @@ def section_handler(words):
    else: invokeError("An invalid opcode was detected.")
 
 
+# Public Functions
+def first_step(assembly_file):
+   with open(assembly_file,'r') as origin_file:
+      all_lines = origin_file.readlines()
+      for index, line in enumerate(all_lines):
+         if line == '\n' or line[0] == ';': continue                                      # Blank lines, Comment lines
+         all_lines[index] = translate_line(line[:-1] if line[-1] is '\n' else line)       # remove \n
+   with open('semi_output.txt', 'w') as outfile:
+      for line in all_lines:
+         outfile.write(line + '\n')
 
-first_step('lightAssmbly.txt')
-# first_step('Example.txt')
-print(sym_tbl)
-second_step('lightOutput.txt')
-# second_step('Output.txt')
+
+def second_step(outfile_name):
+   with open('semi_output.txt','r') as intermidiate_file:
+      all_lines = intermidiate_file.readlines()
+      for index, line in enumerate(all_lines):
+         all_lines[index] = fix_line(line[:-1] if line[-1] is '\n' else line)          # remove \n
+   with open('cool_' + outfile_name, 'w') as outfile:                                  # Output with line breaks
+      for line in all_lines:
+         outfile.write(line + '\n')
+   with open(outfile_name, 'w') as outfile:
+      for line in all_lines:
+         outfile.write(line)
+   print("Assembling Done.")
+
+
+def assemble_code(input_file, output_file):
+   first_step(input_file)
+   second_step(output_file)
+
+
+""" # For Extreme Case
+assemble_code('lightAssmbly.txt', 'lightOutput.txt') """
+
+# For Example Case
+assemble_code('Example.txt', 'Output.txt')
